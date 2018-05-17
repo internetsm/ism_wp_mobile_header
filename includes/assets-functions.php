c3bf4acbc5c3a1dbda0d614fc10213126c6cd8be
Michele Capicchioni <capimichi@gmail.com>
2018-05-17 10:01:00 +0200

    Added icon 3

--- includes/assets-functions.php
+++ includes/assets-functions.php
@@ -4,8 +4,6 @@ add_action('wp_footer', function () {
     wp_enqueue_style('ism_mobile_header_css', ISMMH_ASSETS_URL . "/ism-mobile-header.css");
 });
 
-add_action('admin_footer', function () {
-    ?>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <?php
+add_action('admin_head', function () {
+    wp_enqueue_style('ism_mobile_header_fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
 });
