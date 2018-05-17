65dd418e6db19b87656abadcb543e65a16d36f35
Michele Capicchioni <capimichi@gmail.com>
2018-05-17 09:56:39 +0200

    Added icon 1

--- includes/assets-functions.php
+++ includes/assets-functions.php
@@ -1,5 +1,6 @@
 <?php
 
 add_action('wp_footer', function () {
+    wp_enqueue_style('ism_mobile_header_fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
     wp_enqueue_style('ism_mobile_header_css', ISMMH_ASSETS_URL . "/ism-mobile-header.css");
 });
